c41f93d15f41eb318112dd665669edfe987936ec
wlsql852 <wlsql852@naver.com>
2024-08-20 01:12:05 +0900

    Query Method

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -2,8 +2,14 @@ package com.sparta.memo.repository;
 
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+    //수정일 내림차순 기준으로 모든 메모 조회
+    List<Memo> findAllByOrderByModifiedAtDesc();
+
+    //해당 username을 가진 데이터 조회
+    List<Memo> findAllByUsername(String username);
 
 }
